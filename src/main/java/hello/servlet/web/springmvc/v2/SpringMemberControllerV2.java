472f4361239c0f1d4fb76579df59554489b31212
OSEUK <tpdnrzmzm3@naver.com>
2024-01-31 12:00:43 +0900

    2024-01-31

---
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.springmvc.v2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringMemberFormControllerV2 {
+
+    @RequestMapping("/springmvc/v2/members/new-form")
+}
